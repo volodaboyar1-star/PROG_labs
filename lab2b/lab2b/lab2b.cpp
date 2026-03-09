a94bf534358914c65b86981ff7abb5e318aaa53c
Volodymyr Boiar <volodaboyar1@gmail.com>
2026-03-09 09:17:21 +0200

    backup lab2b

---
+++ lab2b/lab2b/lab2b.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "struct.h"
+#include "print.h"
+
+int main() {
+	int mode;
+	cout << "1-INTERACT" << endl;
+	cout << "2-DEMO"<< endl;
+	cout << "3-BENCHMARK" << endl;
+	cin >> mode;
+
+	if (mode == 1) interactive_mode();
+	else if (mode == 2) demo_mode();
+	else if (mode == 3) benchmark_mode();
+	else cout << "Завершення роботи" << endl;
+
+	return 0;
+}
